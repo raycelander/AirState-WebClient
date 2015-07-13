756385dfec84a6f7fbbfab506db5000a0def28c5
Rainer Hirter <r.hirter@gmail.com>
2015-07-13 16:55:24 +0200

    Preparing for min/max values.

--- Airstate/js/measurements.js
+++ Airstate/js/measurements.js
@@ -1,20 +1,14 @@
 var restUrl;
 var data;
 
-var Measurements = function(){ };
-
-Measurements.prototype.getMeasurementsByStation = function(station){
-    restUrl = "https://airstate.firebaseio.com/measurements/"+station+".json";
-    
-    var http = new XMLHttpRequest();  
-    http.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-              data = JSON.parse(this.responseText);
-       }
-    };
-    http.open("GET", restUrl,false);
-    http.send();
-    return data;
+var Measurements = function(){
+    return{
+        getCurrent : Measurements.prototype.getCurrent,
+        getMeasurementsByStation : Measurements.prototype.getMeasurementsByStation,
+        getMeasurements : Measurements.prototype.getMeasurements,
+        getMaxTemp : Measurements.prototype.getMaxTemp,
+        getMinTemp : Measurements.prototype.getMinTemp
+    }
 };
 
 Measurements.prototype.getMeasurements = function(){
@@ -31,3 +25,31 @@ Measurements.prototype.getMeasurements = function(){
     return data;
 };
 
+Measurements.prototype.getCurrent = function(station){
+    var restDay = new Date().yyyymmdd();
+    restUrl = "https://airstate.firebaseio.com/measurements/"+station+ "/" + restDay + ".json";
+    
+    var http = new XMLHttpRequest();  
+    http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+              data = JSON.parse(this.responseText);
+       }
+    };
+    http.open("GET", restUrl,false);
+    http.send();
+    
+    var lastKey = Object.keys(data).sort().reverse()[0];
+    var lastValue = data[lastKey];
+    if (lastValue.temp.toString().indexOf(".") < 0){
+        lastValue.temp +=".00";
+    }
+    return lastValue;
+};
+
+Measurements.prototype.getMaxTemp = function(station){
+    return "20.2";
+}
+
+Measurements.prototype.getMinTemp = function(station){
+    return "2.4";
+}
--- Airstate/jsx/pom-component.jsx
+++ Airstate/jsx/pom-component.jsx
@@ -2,22 +2,76 @@ var PointOfMeasureComponent = React.createClass({
 
     getInitialState: function(){
        return {
-         currentData: Object
+         currentData: Object,
        }
     },
     componentWillMount: function () {
-        var c = new Measurement().getCurrent(this.props.station);
+        var m = new Measurements();
+        var c = m.getCurrent(this.props.station);
         var temp =  c["temp"].toString();
         var tempPre = temp.substring(0,temp.indexOf("."));
         var tempPost = temp.substring(temp.indexOf(".")+1);
-        var hum =   c["hum"];
+        var tempMax = m.getMaxTemp(this.props.station);
+        var tempMin = m.getMinTemp(this.props.station);
+        var hum =   c["hum"].toFixed(0).toString();
+       
         this.setState({ currentTempPre: tempPre,
                         currentTempPost: tempPost,
                         currentHum: hum,
-                        });
+                        displayTempPre: 0,
+                        displayTempPost: 0,
+                        displayTempMax: tempMax,
+                        displayTempMin: tempMin,
+                        displayHum: 0,
+                        defaultView: true
+                    });
     },
     componentDidMount : function(){
-      var options = {
+        this.countUp();
+    },
+    getDefaultProps: function () {
+        return {
+            station: ''
+        }
+    },
+    render: function () {
+        return (
+            <div className="col-xs-6 col-sm-3 thermometer">
+                <div className="de">
+                    <div className="den">
+                      <div className="dene">
+                        <div className="denem">
+                          <div className="denemee">{this.props.station}</div>
+                          <div className="deneme" onClick={this.handleClick}>
+                                { this.state.defaultView ?
+                                    <div>  
+                                        <div className="value">
+                                            <span className="big" id="currentTempPre">{this.state.currentTempPre}</span>
+                                            <span className="post">.</span>
+                                            <span className="post" id="currentTempPost">{this.state.currentTempPost}</span>
+                                            <span className="unit">&nbsp;&deg;C</span>
+                                        </div>
+                                        <div className="denemen">
+                                          <span id="currentHum">{this.state.displayHum}</span><span> %</span>
+                                        </div>
+                                    </div>
+                               : <div className="small">
+                                    <div>max:&nbsp;<b>{this.state.displayTempMax}°C</b>&nbsp;12:23</div>
+                                    <div>min:&nbsp;<b>{this.state.displayTempMin}</b>°C&nbsp;2:23</div>
+                               </div> }
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        );
+    },
+	refresh: function(){
+		console.log("refreshing");
+	},
+    countUp: function(){
+         var options = {
               useEasing : true, 
               useGrouping : true, 
               separator : ',', 
@@ -35,40 +89,12 @@ var PointOfMeasureComponent = React.createClass({
              clearInterval(refreshIntervalId);
          }, 500);
     },
-    getDefaultProps: function () {
-        return {
-            station: ''
+    handleClick: function(event) {
+        this.setState({defaultView : !this.state.defaultView});
+        if (this.state.defaultView){
+            this.setState({displayHum: this.state.currentHum});
+            this.setState({displayTempPre: this.state.currentTempPre});
+            this.setState({displayTempPost: this.state.currentTempPost});
         }
     },
-    render: function () {
-        var fontStyle = {
-          fontSize: '36px'
-        };
-        var stationStyle={
-            fontSize:'24px',
-        }
-
-        return (
-            <div className="col-xs-6 col-sm-3 thermometer">
-                <div className="de">
-                    <div className="den">
-                      <div className="dene">
-                        <div className="denem">
-                          <div className="denemee">{this.props.station}</div>
-                          <div className="deneme">
-                            <span className="big" id="currentTempPre">0</span>.<span className="post" id="currentTempPost">0</span><strong>&deg;</strong>
-                            <div className="denemen">
-                              <span id="currentHum">0</span><span> %</span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-            </div>
-        );
-    },
-	refresh: function(){
-		console.log("refreshing");
-	}
 });
